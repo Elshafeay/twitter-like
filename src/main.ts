4cd507c6c807db95bff9d1a2b211656d938eeaf7
Mohamed Elshafeay <mohamedelshafeay@gmail.com>
2020-10-07 16:08:41 +0200

    some configurations for deployment (heroku)

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,8 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './Modules/app/app.module';
 
 async function bootstrap() {
-	const app = await NestFactory.create(AppModule, { logger: false });
-	await app.listen(3000);
+	const app = await NestFactory.create(AppModule);
+	app.enableCors();
+	await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
